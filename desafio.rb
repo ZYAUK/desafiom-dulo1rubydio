c73bfaaf6257ea2ca73e2594c4e2ff981c508595
Andrew Bueno Peter <139630427+ZYAUK@users.noreply.github.com>
2024-07-03 21:40:09 -0300

    Add files via upload

---
+++ desafio.rb
@@ -0,0 +1,15 @@
+#o usuário deverá escrever seu nome
+print "Digite seu nome:"
+nome = gets.chomp
+#o usuário deverá escrever seu sobrenome
+print "Digite seu sobrenome:"
+sobrenome = gets.chomp
+#sua idade
+print "Digite sua idade:"
+idade = gets.chomp
+
+puts "Olá #{nome} #{sobrenome}!"
+puts "Eu também tenho #{idade} anos!" 
+
+#Desafio do primeiro módulo do curso da DIO!!
+
